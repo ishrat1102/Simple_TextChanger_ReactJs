c63274207df0b5e97364bea16fda03ad8343ed22
Ishrat Jaben Bushra <83410344+ishrat1102@users.noreply.github.com>
2022-02-15 12:34:32 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-import About from './components/About';
+
 
 
 function App() {
   return (
     <>
-      <Navbar   />
-      {/* jodi default use kori ekta na likhleo hoi*/ }
+      <Navbar  />
       < div className="container my-2" >
       <Textform heading="input text"/>
-      {/*<About/>*/}
       </div>
     </>
   );
